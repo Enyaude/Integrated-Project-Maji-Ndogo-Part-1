3e288840619362b5103cf67aaf11115557816b54
Samuel-Enyaude <enyaudesamuel@hotmail.com>
2024-08-19 05:01:42 +0100

    First commit

---
+++ Setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Integrated-Project-Maji-Ndogo-Part-1',
+    version='0.1',
+    license='MIT',
+    description='EDSA example SQL package',
+    install_requires=['SQL', 'JupyterNotebook'],
+    url='https://github.com/Enyaude/Integrated-Project-Maji-Ndogo-Part-1.git',
+    author='<Enyaude>',
+    author_email='<enyaudesamuel@hotmail.com>'
+    )
